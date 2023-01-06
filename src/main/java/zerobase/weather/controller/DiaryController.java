b95c935cfca4bc095ed156ebe15ec782051c71db
YujeongSon <yujeongs24@naver.com>
2023-01-06 16:20:44 +0900

    날씨 일기 저장 API 구현

---
+++ src/main/java/zerobase/weather/controller/DiaryController.java
@@ -0,0 +1,24 @@
+package zerobase.weather.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import zerobase.weather.service.DiaryService;
+
+import java.time.LocalDate;
+
+@RequiredArgsConstructor
+@RestController
+public class DiaryController {
+
+    private final DiaryService diaryService;
+
+    @PostMapping("/create/diary")
+    void createDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date // yyyy-MM-dd
+            , @RequestBody String text) {
+        diaryService.createDiary(date, text);
+    }
+}
